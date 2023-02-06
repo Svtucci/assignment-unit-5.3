61d058d9b7096e35dcfbbe9dd7e0ed2720285296
Stephen Vertucci <steve.vertucci@gmail.com>
2023-02-06 12:57:08 -0500

    partially finished first few questions, setup empty array, added first funciton, then added 1 object to array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,15 @@
 console.log('***** Music Collection *****')
+
+let collection = [
+    {
+        title: 
+        artist:
+        YearPublished:
+    },
+];
+
+
+
+function addToCollection (title, artist, YearPublished) {
+
+}
