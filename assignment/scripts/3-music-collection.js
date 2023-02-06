fc47c050c0708258cf74d6b935e08827262e47c7
Stephen Vertucci <steve.vertucci@gmail.com>
2023-02-06 14:17:07 -0500

    Still sending empty array and not adding objects

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,15 +1,35 @@
 console.log('***** Music Collection *****')
 
-let collection = [
-    {
-        title: 
-        artist:
-        YearPublished:
-    },
-];
+let collection = [];
+
+/** 
+function addToCollection (title, artist, yearPublished) {
+    collection.title = title;
+    collection.artist = artist;
+    collection.yearPublished = yearPublished;
+} 
+*/
+
+
+let albumOne = {
+    title: 'Immutable',
+    artist: 'Meshuggah',
+    yearPublished: 2022,
+};
+
+let albumTwo = {
+    title: 'RazorsEdge', 
+    artist: 'ACDC',
+    yearPublished: 1990, 
+};
 
 
 
-function addToCollection (title, artist, YearPublished) {
 
-}
+function addToCollection (albumOne) {
+    collection.push(albumOne);
+};
+
+
+console.log(addToCollection);
+console.log(collection);
