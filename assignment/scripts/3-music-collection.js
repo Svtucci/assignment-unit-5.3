1181b6d13224d9aa591ed36593c1cfe6ed939e6f
Stephen Vertucci <steve.vertucci@gmail.com>
2023-02-06 14:25:15 -0500

    Redid function to call parameters within function, and add parameters via console.log

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,34 +2,16 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-/** 
+
 function addToCollection (title, artist, yearPublished) {
-    collection.title = title;
-    collection.artist = artist;
-    collection.yearPublished = yearPublished;
-} 
-*/
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    } 
+    collection.push(album);
+    return album
+}
 
+console.log(addToCollection('Umbrella', 'Rihanna', 2010));
 
-let albumOne = {
-    title: 'Immutable',
-    artist: 'Meshuggah',
-    yearPublished: 2022,
-};
-
-let albumTwo = {
-    title: 'RazorsEdge', 
-    artist: 'ACDC',
-    yearPublished: 1990, 
-};
-
-
-
-
-function addToCollection (albumOne) {
-    collection.push(albumOne);
-};
-
-
-console.log(addToCollection);
-console.log(collection);
